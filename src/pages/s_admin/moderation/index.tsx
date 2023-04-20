4972b90468bdfcf37f3c7f861ee20401158ac804
Krom_oS1 <Roma@bezkassira.by>
2023-04-20 22:00:22 +0300

    feat: add new routes

---
+++ src/pages/s_admin/moderation/index.tsx
@@ -0,0 +1,9 @@
+import { NextPageWithLayout, getLayout } from '@/App'
+
+const ModerationPage: NextPageWithLayout = () => {
+  return <>Moderation</>
+}
+
+ModerationPage.getLayout = getLayout
+
+export default ModerationPage
---
+++ src/pages/s_admin/schools/index.tsx
@@ -0,0 +1,9 @@
+import { NextPageWithLayout, getLayout } from '@/App'
+
+const SchoolsPage: NextPageWithLayout = () => {
+  return <>SchoolsPage</>
+}
+
+SchoolsPage.getLayout = getLayout
+
+export default SchoolsPage
